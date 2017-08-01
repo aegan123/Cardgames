d695a87d962d4dde5ec481db0872fd36ebf823bf
aegan123 <tjmvah@utu.fi>
2017-08-01 21:37:02 +0300

    Edit copyright info

--- src/cardgames/Dealer.java
+++ src/cardgames/Dealer.java
@@ -1,6 +1,6 @@
 /*
 	Class representing the dealer in cardgames
-    Copyright (C) 2017  Juhani Vähä-Mäkilä
+    Copyright (C) 2017  Juhani Vähä-Mäkilä, juhani@fmail.co.uk
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
